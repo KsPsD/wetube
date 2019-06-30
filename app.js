28662e80b3b0ee766fe6eacb37d2491cb500bc93
daechul <44284694+KsPsD@users.noreply.github.com>
2019-06-30 11:38:13 +0900

    routes and contorller

--- app.js
+++ app.js
@@ -3,11 +3,13 @@ import morgan from"morgan";
 import cookieParser from "cookie-parser";
 import helmet from "helmet";
 import bodyParser from "body-parser";
-import  {userRouter} from "./router.js";
+import userRouter from "./routers/userRouter";
+import videoRouter from "./routers/videoRouter";
+import globalRouter from "./routers/globalRouter";
+import routes from "./routes"
+
 const app = express();
 
-const handleHome=(req,res)=>res.send("welcome my home")
-const handleProfile=(req,res)=>res.send("this is profile")
 
 
 app.use(cookieParser());
@@ -16,8 +18,10 @@ app.use(bodyParser.urlencoded({extended:true}));
 app.use(helmet())
 app.use(morgan("combined"));
 
-app.get("/",handleHome); 
-app.get("/profile",handleProfile);
+app.use(routes.home, globalRouter);
+app.use(routes.users, userRouter);
+app.use(routes.videos,videoRouter);
 
-app.use("/user",userRouter);
+
+
 export default app;
