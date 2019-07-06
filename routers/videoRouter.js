2816e900fda83f995f818aaa19f81a30a71703c4
daechul <44284694+KsPsD@users.noreply.github.com>
2019-07-07 00:41:56 +0900

    upload get and post method

--- routers/videoRouter.js
+++ routers/videoRouter.js
@@ -3,7 +3,8 @@ import routes from "../routes";
 
 import {
     videos,
-    upload,
+    getUpload,
+    postUpload,
     videoDetail,
     editVideo,
     deleteVideo,
@@ -14,9 +15,10 @@ import {
 const videoRouter = express.Router();
 
 
-videoRouter.get(routes.upload,upload);
+videoRouter.get(routes.upload,getUpload);
+videoRouter.post(routes.upload,postUpload);
 videoRouter.get(routes.editVideo,editVideo);
-videoRouter.get(routes.videoDetail,videoDetail);
+videoRouter.get(routes.videoDetail(),videoDetail);
 
 videoRouter.get(routes.deleteVideo,deleteVideo);
 
