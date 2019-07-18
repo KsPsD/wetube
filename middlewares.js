1f0f1d5280d650e9e4c56dce0cc48520d4128c9e
daechul <44284694+KsPsD@users.noreply.github.com>
2019-07-18 23:34:06 +0900

    lots of scss

--- middlewares.js
+++ middlewares.js
@@ -7,7 +7,7 @@ export const localMiddleware = (req, res, next) => {
   res.locals.siteName = "WeTube";
   res.locals.routes = routes;
   res.locals.user = {
-    isAuthenticated: true,
+    isAuthenticated: false,
     id: 1
   };
   next(); // 잊어버리지 마라 미들웨어 하고 넘겨줘야됨
